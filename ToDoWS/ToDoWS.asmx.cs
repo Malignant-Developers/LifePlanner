97f4ffeb1519aa520b7f614302c92eeced46d70d
Jeremy @Desktop <jeremyzelaya@hotmail.es>
2019-11-26 02:22:12 -0600

    Merged activities and changed WS

--- ToDoWS/ToDoWS.asmx.cs
+++ ToDoWS/ToDoWS.asmx.cs
@@ -20,11 +20,8 @@ namespace ToDoWS
     // [System.Web.Script.Services.ScriptService]
     public class ToDoWS : System.Web.Services.WebService
     {
-        // Conexion al servidor donde se almacena la base de datos: http://freeasphosting.net
-        // Usuario: todolistappprgv
-        // Contraseña:QtKZ1D$w64Uy
         // En un string no se puede usar un solo '\' ya que es usado para expresiones como \n, sino que se deben usar dos '\\' para indicar su uso: https://stackoverflow.com/questions/18532691/how-do-i-write-a-backslash-in-a-string
-        static public string connectionString = "Server=sql.freeasphost.net\\MSSQL2016;Database=todolistappprgv_ToDoList;uid=todolistappprgv;pwd=admin;";
+        static public string connectionString = "workstation id=LifePlanner.mssql.somee.com;packet size=4096;user id=TodoAppWS_SQLLogin_1;pwd=2agolwe6t3;data source=LifePlanner.mssql.somee.com;persist security info=False;initial catalog=LifePlanner";
         static SqlConnection conexion = new SqlConnection(connectionString);
 
         public DataTable EjecutarConsulta(StringBuilder query, SqlCommand comando = null)
@@ -85,52 +82,24 @@ namespace ToDoWS
         }
         // Create
         [WebMethod]
-        public int CrearTelefono(int telefono, string descripcion)
+        public int CrearUsario(string nombre, string correo, int telefono)
         {
             int resultado;
             SqlCommand comando = new SqlCommand();
             StringBuilder query = new StringBuilder();
             // Comando
-            query.Append("INSERT INTO Telefono VALUES(@telefono, @descripcion)");
+            query.Append("INSERT INTO usuario VALUES(@nombre, @correo, @telefono)");
             // Parametros
-            comando.Parameters.Add("@telefono", SqlDbType.Int).Value = telefono;
-            comando.Parameters.Add("@descripcion", SqlDbType.NVarChar).Value = descripcion;
-            
-            resultado = EjecutarSentencia(query, comando);
-            return resultado;
-        }
-        [WebMethod]
-        public int CrearCorreo(string correo, string descripcion)
-        {
-            int resultado;
-            SqlCommand comando = new SqlCommand();
-            StringBuilder query = new StringBuilder();
-            // Comando
-            query.Append("INSERT INTO Correo VALUES(@correo, @descripcion)");
-            // Parametros
+            comando.Parameters.Add("@nombre", SqlDbType.NVarChar).Value = nombre;
             comando.Parameters.Add("@correo", SqlDbType.NVarChar).Value = correo;
-            comando.Parameters.Add("@descripcion", SqlDbType.NVarChar).Value = descripcion;
-
-            resultado = EjecutarSentencia(query, comando);
-            return resultado;
-        }
-        [WebMethod]
-        public int CrearTarea(string descripcion)
-        {
-            int resultado;
-            SqlCommand comando = new SqlCommand();
-            StringBuilder query = new StringBuilder();
-            // Comando
-            query.Append("INSERT INTO Tarea VALUES(@descripcion)");
-            // Parametros
-            comando.Parameters.Add("@descripcion", SqlDbType.NVarChar).Value = descripcion;
-
+            comando.Parameters.Add("@telefono", SqlDbType.Int).Value = telefono;
+            
             resultado = EjecutarSentencia(query, comando);
             return resultado;
         }
         // Obtener
         [WebMethod]
-        public DataTable ObtenerTelefonos()
+        public DataTable ObtenerUsuario()
         {
             // Obtener todos los contactos
             DataTable tabla = new DataTable();
@@ -138,128 +107,42 @@ namespace ToDoWS
             // Usar string builder por cuestiones de rendimiento
             StringBuilder query = new StringBuilder();
             // Comando
-            query.Append("SELECT * FROM Telefono");
+            query.Append("SELECT * FROM Usuario");
 
             tabla = EjecutarConsulta(query, comando);
             return tabla;
         }
-        [WebMethod]
-        public DataTable ObtenerCorreos()
-        {
-            // Obtener todos los correos
-            DataTable tabla = new DataTable();
-            SqlCommand comando = new SqlCommand();
-            // Usar string builder por cuestiones de rendimiento
-            StringBuilder query = new StringBuilder();
-            // Comando
-            query.Append("SELECT * FROM Correo");
-
-            tabla = EjecutarConsulta(query, comando);
-            return tabla;
-        }
-        [WebMethod]
-        public DataTable ObtenerTareas()
-        {
-            // Obtener todas las tareas
-            DataTable tabla = new DataTable();
-            SqlCommand comando = new SqlCommand();
-            // Usar string builder por cuestiones de rendimiento
-            StringBuilder query = new StringBuilder();
-            // Comando
-            query.Append("SELECT * FROM Tarea");
-
-            tabla = EjecutarConsulta(query, comando);
-            return tabla;
-        }
-        
 
         // Actualizar
         [WebMethod]
-        public int ActualizarTelefono(int id_telefono,int telefono, string descripcion)
+        public int ActualizarUsuario(int id, string nombre, string correo, int telefono)
         {
             int resultado;
             SqlCommand comando = new SqlCommand();
             StringBuilder query = new StringBuilder();
             // Comando
-            query.Append("UPDATE Telefono SET Telefono = @telefono, Descripcion = @descripcion WHERE ID_Telefono = @id_telefono");
+            query.Append("UPDATE Usuario SET Nombre = @nombre, Correo = @correo, Telefono = @telefono WHERE Id = @id");
             // Parametros
-            comando.Parameters.Add("@id_telefono", SqlDbType.Int).Value = id_telefono;
-            comando.Parameters.Add("@telefono", SqlDbType.Int).Value = telefono;
-            comando.Parameters.Add("@descripcion", SqlDbType.NVarChar).Value = descripcion;
-
-            resultado = EjecutarSentencia(query, comando);
-            return resultado;
-        }
-        [WebMethod]
-        public int ActualizarCorreo(int id_correo, string correo, string descripcion)
-        {
-            int resultado;
-            SqlCommand comando = new SqlCommand();
-            StringBuilder query = new StringBuilder();
-            // Comando
-            query.Append("UPDATE Correo SET Correo = @correo, Descripcion = @descripcion WHERE ID_Correo = @id_correo");
-            // Parametros
-            comando.Parameters.Add("@id_correo", SqlDbType.Int).Value = id_correo;
+            comando.Parameters.Add("@id", SqlDbType.Int).Value = id;
+            comando.Parameters.Add("@nombre", SqlDbType.NVarChar).Value = nombre;
             comando.Parameters.Add("@correo", SqlDbType.NVarChar).Value = correo;
-            comando.Parameters.Add("@descripcion", SqlDbType.NVarChar).Value = descripcion;
+            comando.Parameters.Add("@telefono", SqlDbType.Int).Value = telefono;
 
             resultado = EjecutarSentencia(query, comando);
             return resultado;
         }
-        [WebMethod]
-        public int ActualizarTarea(int id_tarea, string tarea)
-        {
-            int resultado;
-            SqlCommand comando = new SqlCommand();
-            StringBuilder query = new StringBuilder();
-            // Comando
-            query.Append("UPDATE Tarea SET Tarea = @tarea WHERE ID_Tarea = @id_tarea");
-            // Parametros
-            comando.Parameters.Add("@id_tarea", SqlDbType.Int).Value = id_tarea;
-            comando.Parameters.Add("@tarea", SqlDbType.NVarChar).Value = tarea;
-
-            resultado = EjecutarSentencia(query, comando);
-            return resultado;
-        }
+      
         // Borrar
         [WebMethod]
-        public int BorrarTelefono(int id_telefono)
+        public int BorrarUsuario(int id)
         {
             int resultado;
             SqlCommand comando = new SqlCommand();
             StringBuilder query = new StringBuilder();
             // Comando
-            query.Append("DELETE FROM Telefono WHERE ID_Telefono = @id_telefono");
+            query.Append("DELETE FROM Usuario WHERE Id = @id");
             // Parametros
-            comando.Parameters.Add("@id_telefono", SqlDbType.Int).Value = id_telefono;
-
-            resultado = EjecutarSentencia(query, comando);
-            return resultado;
-        }
-        [WebMethod]
-        public int BorrarCorreo(int id_correo)
-        {
-            int resultado;
-            SqlCommand comando = new SqlCommand();
-            StringBuilder query = new StringBuilder();
-            // Comando
-            query.Append("DELETE FROM Correo WHERE ID_Correo = @id_correo");
-            // Parametros
-            comando.Parameters.Add("@id_correo", SqlDbType.Int).Value = id_correo;
-
-            resultado = EjecutarSentencia(query, comando);
-            return resultado;
-        }
-        [WebMethod]
-        public int BorrarTarea(int id_tarea)
-        {
-            int resultado;
-            SqlCommand comando = new SqlCommand();
-            StringBuilder query = new StringBuilder();
-            // Comando
-            query.Append("DELETE FROM Tarea WHERE ID_Tarea = @id_tarea");
-            // Parametros
-            comando.Parameters.Add("@id_tarea", SqlDbType.Int).Value = id_tarea;
+            comando.Parameters.Add("@id", SqlDbType.Int).Value = id;
 
             resultado = EjecutarSentencia(query, comando);
             return resultado;
